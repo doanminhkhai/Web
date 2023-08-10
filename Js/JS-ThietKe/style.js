1d29e7e038f00992ac35262770afd2b2f2c05148
doanminhkhai <bxgangter@gmail.com>
2023-08-10 14:40:16 +0700

    ..

---
+++ Js/JS-ThietKe/style.js
@@ -0,0 +1,25 @@
+const slider = document.querySelector('.slider');
+let isDown = false;
+let startX, scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; // thiết lập tốc độ cuộn trang
+  slider.scrollLeft = scrollLeft - walk;
+});
